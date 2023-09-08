9cd84ed8464887c3d2f46e5e38221733fe44ad91
Lucas Balon <lucasbalon@gmail.com>
2023-09-08 11:37:00 +0200

    suppression script dans html en créant function generateLi()

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 let taches = [];
-
+generateLi();
 function addTache(id) {
-    let title = document.getElementById("").value;
+    let title = document.getElementById("title").value;
     let description = document.getElementById("").value;
 
     let task = {
@@ -13,7 +13,34 @@ function addTache(id) {
 
     task.title = title;
     task.description = description;
-    task.id = id;
+    task.day = id;
 
     taches.push(task);
+}
+
+function generateLi() {
+    let el, a;
+    for( var _i = 1; _i <= 31; _i += 1 ){
+
+        el = document.createElement("li");
+        a = document.createElement("a");
+        a.title = _i;
+        a.id = _i;
+        a.innerText = _i;
+        a.setAttribute("data-value", `${_i}`);
+
+        if( _i === 12 ){ a.classList.add("selected"); }
+
+        switch( _i ){
+            case 8:
+            case 10:
+            case 27:
+                a.classList.add("event");
+                break;
+        }
+
+        el.append(a);
+
+        document.getElementsByClassName("days").item(0).append(el);
+    }
 }
