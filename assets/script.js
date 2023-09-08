ede8dbba551d57bb9e0bfce324b74219ace8a813
Jacoby Ismail <ismail.jacoby@gmail.com>
2023-09-08 11:24:08 +0200

    Ajout de la fonction pour selectionner les dates differentes

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,23 @@
+// Get all the date elements with class "event"
+var dateElements = document.querySelectorAll('.days a');
+// Add a click event to each date element
+dateElements.forEach(function (dateElement) {
+    dateElement.addEventListener('click', function (event) {
+        // Prevent the default link behavior
+        event.preventDefault();
+
+        // Remove the "selected" class from the previously selected date
+        var selectedDate = document.querySelector('.days a.selected');
+        if (selectedDate) {
+            selectedDate.classList.remove('selected');
+        }
+
+        // Add the "selected" class to the clicked date
+        dateElement.classList.add('selected');
+    });
+});
+
+
 let taches = [];
 
 function addTache(id) {
