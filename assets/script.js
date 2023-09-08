4c7fd6d4bf6e881f3311566cc1d8e6a4c5eaaab1
Jacoby Ismail <ismail.jacoby@gmail.com>
2023-09-08 11:46:23 +0200

    Modifie la couleur des dates qui ont des taches

--- assets/script.js
+++ assets/script.js
@@ -10,10 +10,20 @@ dateElements.forEach(function (dateElement) {
         var selectedDate = document.querySelector('.days a.selected');
         if (selectedDate) {
             selectedDate.classList.remove('selected');
+
+            // Check if the previously selected date had both "event" and "selected" classes
+            if (selectedDate.classList.contains('event')) {
+                selectedDate.style.color = '#F48989'; // Change font color back to #F48989 for selected events
+            }
         }
 
         // Add the "selected" class to the clicked date
         dateElement.classList.add('selected');
+
+        // Check if the clicked date has both "event" and "selected" classes
+        if (dateElement.classList.contains('event')) {
+            dateElement.style.color = 'white'; // Change font color to white for selected events
+        }
     });
 });
 
